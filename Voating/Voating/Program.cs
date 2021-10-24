213e73cf793289393485bafc8f7f530791f88b29
ScMohanty <92923028+ScMohanty@users.noreply.github.com>
2021-10-24 11:00:49 +0530

    Add files via upload

---
+++ Voating/Voating/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dob;
+            DateTime today = DateTime.Now;
+
+            Console.WriteLine("ENTER DATE OF BIRTH PLEASE");
+            dob = Convert.ToDateTime(Console.ReadLine());
+
+            int age = today.Year - dob.Year;
+
+            if (age >= 18)
+            {
+                Console.WriteLine("The persion is eligible for voating");
+            }
+            else
+            {
+                Console.WriteLine("the persion is not eligible for voating");
+            }
+        }
+    }
+}
